934940350a911f78cde29a4b8dcd6acc839d0a2c
Alfonso Tamés <alfonso@tames.com>
2020-06-30 21:13:53 -0500

    Added itsybitsy_m0 board

---
+++ boards/arm/itsybitsy_m0/pinmux.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2018 Bryan O'Donoghue
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <drivers/pinmux.h>
+#include <soc.h>
+
+static int board_pinmux_init(struct device *dev)
+{
+	struct device *muxa = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_a)));
+	struct device *muxb = device_get_binding(DT_LABEL(DT_NODELABEL(pinmux_b)));
+
+	ARG_UNUSED(dev);
+
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_uart) && CONFIG_UART_SAM0)
+	/* SERCOM0 on RX=PA11, TX=PA10 */
+	pinmux_pin_set(muxa, 11, PINMUX_FUNC_C);
+	pinmux_pin_set(muxa, 10, PINMUX_FUNC_C);
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_uart) && CONFIG_UART_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_uart) && CONFIG_UART_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_uart) && CONFIG_UART_SAM0)
+pinmux_pin_set(muxa, 25, PINMUX_FUNC_C);
+pinmux_pin_set(muxa, 24, PINMUX_FUNC_C);
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_uart) && CONFIG_UART_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_uart) && CONFIG_UART_SAM0)
+#warning Pin mapping may not be configured
+#endif
+
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+pinmux_pin_set(muxa, 0, PINMUX_FUNC_D);
+pinmux_pin_set(muxa, 1, PINMUX_FUNC_D);
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+/* SPI SERCOM4 on MISO=PB16/pad 0, MOSI=PB22/pad 2, SCK=PB23/pad 3 */
+pinmux_pin_set(muxb, 10, PINMUX_FUNC_D);
+pinmux_pin_set(muxb, 11, PINMUX_FUNC_D);
+pinmux_pin_set(muxa, 12, PINMUX_FUNC_D);
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_spi) && CONFIG_SPI_SAM0)
+#warning Pin mapping may not be configured
+#endif
+
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+#warning Pin mapping may not be configured
+#endif
+#if (ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_i2c) && CONFIG_I2C_SAM0)
+/* SERCOM5 */
+pinmux_pin_set(muxa, 22, PINMUX_FUNC_D);
+pinmux_pin_set(muxa, 23, PINMUX_FUNC_D);
+#endif
+
+#ifdef CONFIG_USB_DC_SAM0
+	/* USB DP on PA25, USB DM on PA24 */
+	pinmux_pin_set(muxa, 25, PINMUX_FUNC_G);
+	pinmux_pin_set(muxa, 24, PINMUX_FUNC_G);
+#endif
+	return 0;
+}
+
+SYS_INIT(board_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
